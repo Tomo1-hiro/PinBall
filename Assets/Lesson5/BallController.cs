a5a0b777a899299cd95d5722b8f42af58b3a53c7
Tomo1-hiro <75882793+Tomo1-hiro@users.noreply.github.com>
2021-01-20 23:04:52 +0900

    Initial commit

---
+++ Assets/Lesson5/BallController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+    
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+    void Start()
+        
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+        if (tag == "SmallStarTag")
+        {
+            this.scoreText.GetComponent<Text>().text = "5";
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.scoreText.GetComponent<Text>().text = "10";
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.scoreText.GetComponent<Text>().text = "20";
+        }
+       
+    }
+    
+}
---
+++ Assets/Lesson5/CloudController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float minimum = 1.0f;
+    private float magSpeed = 2.0f;
+    private float magnification = 0.07f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale = new Vector3(this.minimum + Mathf.Sin(Time.time * this.magSpeed) * this.magnification, this.transform.localScale.y, this.minimum + Mathf.Sin(Time.time * this.magSpeed) * this.magnification);
+
+    }
+}
